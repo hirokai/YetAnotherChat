907b3c64376294b6da70d3d3bc6cb1d17d80ee5e
Hiro Kai <fullerenec84@gmail.com>
2019-07-10 02:13:05 +0900

    Fix test script

--- client/run_elm_matrix.sh
+++ client/run_elm_matrix.sh
@@ -1,1 +1,1 @@
-elm-live src/Matrix.elm -p 8000 -s matrix.html -- --output /public/js/matrix.elm.min.js --debug 
+elm-live src/Matrix.elm -p 8000 -s matrix.html -- --output public/js/matrix.elm.min.js --debug
