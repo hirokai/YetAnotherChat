6bd72036a73495b106bcfdd8dfc53d8b5e33667d
Hiro Kai <fullerenec84@gmail.com>
2019-08-23 01:21:39 +0900

    Apparently solved random error with encryption

--- client/cryptography.ts
+++ client/cryptography.ts
@@ -37,8 +37,26 @@ const Base64a = {
     }())
 };
 
-const encodingFunc: (Uint8Array) => string = encodeBase64URL
-const decodingFunc: (string) => Uint8Array = decodeBase64URL;
+function ab2str(buf: ArrayBuffer): string {
+    var result = '';
+    if (buf) {
+        var bytes = new Uint8Array(buf);
+        for (var i = 0; i < bytes.byteLength; i++) {
+            result = result + String.fromCharCode(bytes[i]);
+        }
+    }
+    return result;
+}
+function str2ab(str: string): Uint8Array {
+    var bytes = new Uint8Array(str.length);
+    for (var iii = 0; iii < str.length; iii++) {
+        bytes[iii] = str.charCodeAt(iii);
+    }
+    return bytes;
+}
+
+const encodingFunc: (a: Uint8Array) => string = encodeBase64URL2;
+const decodingFunc: (a: string) => Uint8Array = decodeBase64URL2;
 
 // https://qiita.com/tomoyukilabs/items/eac94fdb2d0ca92f443a
 export async function generateKeyPair(exportable = false): Promise<CryptoKeyPair> {
@@ -241,7 +259,7 @@ export async function decrypt(remotePublicKey: CryptoKey, localPrivateKey: Crypt
             // console.log('decrypt start', encryptionKey, remotePublicKey, localPrivateKey, encrypted);
             // AES-GCMによる復号
             return crypto.subtle.decrypt(
-                { name: 'AES-GCM', iv: decodeBase64URL(encrypted.iv), tagLength: 128 },
+                { name: 'AES-GCM', iv: decodingFunc(encrypted.iv), tagLength: 128 },
                 encryptionKey,
                 decodingFunc(encrypted.data)
             );
@@ -266,6 +284,36 @@ export async function verify_key_pair({ publicKey, privateKey }: CryptoKeyPair):
     }
 }
 
+function encode2(buf_: ArrayBuffer): string {
+    const buf = new Uint8Array(buf_);
+    let str = '';
+    for (let i = 0; i < buf.byteLength; i++) {
+        const s = buf[i].toString(16);
+        str += buf[i] >= 16 ? ('0' + s) : s;
+    }
+    return str;
+}
+
+function decode2(str: string): Uint8Array {
+    const buf = new Uint8Array(str.length / 2);
+    for (let i = 0; i < str.length; i++) {
+        const n = parseInt(str.slice(i * 2, i * 2 + 2), 16);
+        buf[i] = n;
+    }
+    return buf;
+}
+
+// https://qiita.com/isyumi_net/items/23926604d7d6ccf9a8a5
+function encode1(encMessage: ArrayBuffer): string {
+    return btoa(String.fromCharCode.apply(null, new Uint8Array(encMessage)))
+}
+
+function decode1(src: string): Uint8Array {
+    return new Uint8Array((new Uint16Array([].map.call(src, function (c) {
+        return c.charCodeAt(0)
+    }))).buffer);
+}
+
 // JavaScriptのatob(), btoa()ではURL-safe Base64が扱えないため変換が必要
 export function decodeBase64URL(data: string): Uint8Array {
     let decoded = atob(data.replace(/-/g, '+').replace(/_/g, '/'));
@@ -281,6 +329,22 @@ export function encodeBase64URL(data: Uint8Array): string {
         output += String.fromCharCode(data[i]);
     return btoa(output.replace(/\+/g, '-').replace(/\//g, '_')).replace(/=+$/, '');
 }
+
+export function decodeBase64URL2(data: string): Uint8Array {
+    let decoded = atob(data);
+    let buffer = new Uint8Array(decoded.length);
+    for (let i = 0; i < data.length; i++)
+        buffer[i] = decoded.charCodeAt(i);
+    return buffer;
+}
+
+export function encodeBase64URL2(data: Uint8Array): string {
+    let output = '';
+    for (let i = 0; i < data.length; i++)
+        output += String.fromCharCode(data[i]);
+    return btoa(output);
+}
+
 
 export async function fingerPrint1(key: CryptoKey): Promise<string> {
     const jwk = await exportKey(key).catch(() => null);
