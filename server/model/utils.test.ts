42bd87a188007d7ca457f299de5d72eae446ad78
Hiro Kai <fullerenec84@gmail.com>
2019-08-28 23:04:37 +0900

    Use fast-check

--- server/model/utils.test.ts
+++ server/model/utils.test.ts
@@ -1,23 +1,32 @@
 import * as utils from './utils'
+import * as fc from 'fast-check';
 
 const random_str = (N) => {
     const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     return Array.from(Array(N)).map(() => S[Math.floor(Math.random() * S.length)]).join('');
 };
 
-test('Cipher and decipher', () => {
-    const password = random_str(16);
-    const plain = random_str(16);
-    const ciphered = utils.cipher(plain, password);
-    expect(ciphered).not.toEqual(plain);
-    expect(utils.decipher(ciphered, password)).toEqual(plain);
+test('Cipher and decipher', async done => {
+    await fc.assert(
+        fc.property(fc.fullUnicodeString(100), fc.fullUnicodeString(100), (password, plain) => {
+            const ciphered = utils.cipher(plain, password);
+            const deciphered = utils.decipher(ciphered, password);
+            return plain == deciphered;
+        }),
+        { numRuns: 1000, verbose: false }
+    );
+    done();
 });
 
-test('Cipher and decipher: 2', () => {
-    const password = random_str(16);
-    const plain = random_str(16);
-    const ciphered = utils.cipher2(plain, password);
-    expect(ciphered).not.toEqual(plain);
-    expect(utils.decipher2(ciphered, password)).toEqual(plain);
+test('Cipher and decipher: 2', async done => {
+    await fc.assert(
+        fc.property(fc.fullUnicodeString(100), fc.fullUnicodeString(100), (password, plain) => {
+            const ciphered = utils.cipher2(plain, password);
+            const deciphered = utils.decipher2(ciphered, password);
+            return plain == deciphered;
+        }),
+        { numRuns: 1000, verbose: false }
+    );
+    done();
 });
 
