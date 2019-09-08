51d35c6549be09aa0a03f15f85a97d0fec53c32c
Hiro Kai <fullerenec84@gmail.com>
2019-09-09 08:39:23 +0900

    Add test for registration

--- client/main.test.ts
+++ client/main.test.ts
@@ -1,18 +1,43 @@
 import puppeteer from 'puppeteer';
 
+jest.setTimeout(20000);
 
 describe('Login page', () => {
-    let page;
-    let browser;
+    let page: puppeteer.Page;
+    let browser: puppeteer.Browser;
     beforeAll(async done => {
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({
+            headless: false,  // 動作確認するためheadlessモードにしない
+            slowMo: 100  // 動作確認しやすいようにpuppeteerの操作を遅延させる
+        });
         page = await browser.newPage();
         await page.goto('http://localhost:3000/login');
         done();
     });
-    test('Title', async done => {
+    test('Invalid username should be rejected', async done => {
         await expect(page.title()).resolves.toMatch('ログイン');
-        done();
+        await page.click("a[href='/register']");
+        await page.waitFor(1000);
+        let el = await page.$('h1');
+        if (el == null) {
+            throw new Error('Element not found');
+        } else {
+            const t = await (await el.getProperty('textContent')).jsonValue()
+            expect(t).toEqual('新規登録');
+            await page.type('#username', '__Sato', { delay: 10 });
+            await page.type('#password', '1234', { delay: 10 });
+            await page.click('#agree');
+            await page.click('#submit');
+            el = await page.$('#info-username');
+            if (el == null) {
+                throw new Error('Element not found');
+            } else {
+                const t2 = await (await el.getProperty('textContent')).jsonValue();
+                expect(t2).toEqual('無効なユーザー名です（"__"から始まることはできません）');
+                await page.waitFor(1000);
+                done();
+            }
+        }
     });
     afterAll(async done => {
         await page.close();
